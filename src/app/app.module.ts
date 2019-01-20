09df237b45dbe9fc6d5eefd075e4ebfb2335eeca
mitverma <amitverma951994@gmail.com>
2019-01-20 20:42:59 +0530

    'changes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyApp } from './app.component';
 import { HomePageModule } from '../pages/home/home.module';
+import { HttpClientModule ,HttpHeaders} from "@angular/common/http";
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { ApolloLink, concat } from 'apollo-link';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import {Apollo} from 'apollo-angular';
+
+
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token') || null)
+  });
+
+  return forward(operation);
+});
+
 
 @NgModule({
   declarations: [
-  MyApp,
-  ],
+  MyApp, 
+  ], 
   imports: [
-  BrowserModule,
+  BrowserModule, 
+  HttpClientModule,
+  ApolloModule,
+  HttpLinkModule,
   IonicModule.forRoot(MyApp),
   HomePageModule
   ],
@@ -22,7 +43,22 @@ import { HomePageModule } from '../pages/home/home.module';
   providers: [
   StatusBar,
   SplashScreen,
-  {provide: ErrorHandler, useClass: IonicErrorHandler}
+  HttpLinkModule,
+  HttpLink,
+  Apollo,
+  {provide: ErrorHandler, useClass: IonicErrorHandler},
+  {
+    provide: APOLLO_OPTIONS,
+    useFactory(httpLink: HttpLink) {
+      return {
+        cache: new InMemoryCache(),
+        link: concat(authMiddleware, httpLink.create({
+          uri: "http://www.sunrichrice.com/graphql/"
+        }))
+      }
+    },
+    deps: [HttpLink]
+  }
   ],
 })
 export class AppModule {}
--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag'; 
+import { ApolloClient } from 'apollo-client';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { HttpClient } from '@angular/common/http';
 /**
  * Generated class for the SigninPage page.
  *
@@ -17,7 +21,9 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  })
  export class SigninPage {
  	userDetail: Object = {}; 
- 	constructor(public navCtrl: NavController, public navParams: NavParams) {
+ 	uri: string;
+ 	constructor(public navCtrl: NavController, public navParams: NavParams, public apollo: Apollo) {
+
  	}
 
  	ionViewDidLoad() {
@@ -31,5 +37,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  		if (form.valid) {
  			
  		}
+ 	}
+
+
+ 	getList(){
+ 		this.apollo
+ 		.watchQuery({ 
+ 			query: gql`
+ 			query{
+ 				products {
+ 					edges {
+ 						node {
+ 							id name seoTitle thumbnailUrl description price {
+ 								currency amount
+ 							}
+ 						}
+ 					}
+ 				}}
+ 				`,
+ 			})
+ 		.valueChanges.subscribe(result => {
+ 			console.log(result, 'result');
+ 		});
  	}
  }
